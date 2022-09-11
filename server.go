a55c67ad0b2076140f4a05c7f007bb7dcc0ee42a
stm33 <satuma33@icloud.com>
2022-09-11 18:17:06 +0900

    [add]refs #2-server-respons-view

--- server.go
+++ server.go
@@ -2,20 +2,23 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	hello := []byte("Hello World!!!")
-	_, err := w.Write(hello)
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	html, err := template.ParseFiles("view.html")
 	if err != nil {
+		log.Fatal(err)
+	}
+	if err := html.Execute(w, nil); err != nil {
 		log.Fatal(err)
 	}
 }
 
 func main() {
-	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/view", viewHandler)
 	fmt.Println("Server Start Up........")
 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
